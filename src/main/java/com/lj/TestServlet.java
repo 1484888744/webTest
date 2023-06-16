11135d4bf95c7ef22b255e3751b98d4e422259f1
zhangsp8 <1484888744@qq.com>
2023-06-16 16:12:59 +0800

    初始化项目

---
+++ src/main/java/com/lj/TestServlet.java
@@ -0,0 +1,32 @@
+package com.lj;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Auther:LJ
+ * @Date: 2023/6/13 15:54
+ */
+//@WebServlet("/hello")
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=UTF-8");
+        System.out.println("hello");
+        //super.doGet(req,resp);
+        //req.setAttribute("Title","你好");
+        //req.getRequestDispatcher("/WEB-INF/index.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
